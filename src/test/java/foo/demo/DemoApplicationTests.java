713d8a67a0051be4074d90dd633f056051417377
Brad Solomon <181268661+solomonbrjnih@users.noreply.github.com>
2025-01-05 09:37:32 -0500

    Update using https://stackoverflow.com/a/79330368/7954504

--- src/test/java/foo/demo/DemoApplicationTests.java
+++ src/test/java/foo/demo/DemoApplicationTests.java
@@ -5,12 +5,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Autowired
 	private DemoService service;
+
+	@SpringBootApplication
+	static class TestConfiguration {
+	}
 
 	@Test
 	void contextLoads() throws Exception {
